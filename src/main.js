f8408645835b1779dc7eb87c7a33af73e14135c5
RuxandraRambet <ruxandraramb@gmail.com>
2023-12-20 16:12:03 +0100

    feat: fontawesome and icons

--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // style
 import './assets/scss/general.scss';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app');
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faLinkSlash } from '@fortawesome/free-solid-svg-icons';
+import { faBicycle } from '@fortawesome/free-solid-svg-icons';
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+/* add icons to the library */
+library.add(faLinkSlash, faBicycle, faCloud, faHeart, faFacebook, faTwitter, faInstagram)
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
